ab7c36b549fcfeffc1eb932cd33d245cf235637d
Haufan <118518898+Haufan@users.noreply.github.com>
2025-01-27 08:09:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+
+
+def as_text():
+
+   import spacy
+
+   text = 'John went to the park to meet his friend, Sarah. ' \
+       'She had been waiting for him near the fountain. ' \
+       'When he arrived, he waved to her, and she smiled back. ' \
+       'They decided to walk around and talk about their plans for the weekend. ' \
+       'John mentioned that he wanted to visit the new museum, and Sarah thought it was a great idea. '\
+       'As they walked, Sarah pointed out a group of ducks by the pond. ' \
+       '"Look at them!" she exclaimed. ' \
+       'John laughed and took a picture of the ducks to show his sister later. ' \
+       'He told Sarah that his sister loves animals and would enjoy seeing the photo. ' \
+       'After spending some more time at the park, they said goodbye and promised to meet again soon. ' \
+       'John felt happy about the day, and so did Sarah.'
+
+   nlp = spacy.load("en_core_web_sm")
+   nlp.add_pipe("fastcoref")
+
+   doc = nlp(text)
+   print(doc._.coref_clusters)
+
+   doc = nlp(text, component_cfg={"fastcoref": {'resolve_text': True}})
+   print(doc._.resolved_text)
+
+
+#############################################################################################################
+
+def as_clusters():
+
+   from fastcoref import FCoref
+
+   text = 'John went to the park to meet his friend, Sarah. ' \
+          'She had been waiting for him near the fountain. ' \
+          'When he arrived, he waved to her, and she smiled back. ' \
+          'They decided to walk around and talk about their plans for the weekend. ' \
+          'John mentioned that he wanted to visit the new museum, and Sarah thought it was a great idea. ' \
+          'As they walked, Sarah pointed out a group of ducks by the pond. ' \
+          '"Look at them!" she exclaimed. ' \
+          'John laughed and took a picture of the ducks to show his sister later. ' \
+          'He told Sarah that his sister loves animals and would enjoy seeing the photo. ' \
+          'After spending some more time at the park, they said goodbye and promised to meet again soon. ' \
+          'John felt happy about the day, and so did Sarah.'
+   model = FCoref(device='cpu')
+
+   preds = model.predict(texts=[text])
+
+   print(preds[0].get_clusters(as_strings=False))
+
+   print(preds[0].get_clusters())
+
+   print(preds[0].get_logit(span_i=(0, 4), span_j=(30, 33)))
+
+
+if __name__ == "__main__":
+   #as_text()
+   as_clusters()
